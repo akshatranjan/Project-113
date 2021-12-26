3bfe71453218d29b3aac95db059501593f23b8ce
akshatranjan <73059364+akshatranjan@users.noreply.github.com>
2021-12-26 12:13:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
-function preload() {
+function preload(){
 }
 
 function setup()
 {
-    canvas = createCanvas(640, 480);
-    canvas.position(110, 260);
+    canvas = createCanvas(640,480);
+    canvas.position(90, 170);
     video = createCapture(VIDEO);
-    video.hide();
-
-    tint_colour = "";
+    video.hide()
 }
 
 function draw()
 {
-    image(video, 0, 0, 640, 480);
-    tint(tint_colour);
-}
+    image(video, 150, 150, 300, 200);
 
-function take_snapshot()
-{
-    save("student_name.png");
-}
+    fill(0, 255, 0);
+    circle(60, 60, 60);
+    circle(580, 60, 60);
+    circle(60, 420, 60);
+    circle(580, 420, 60);
 
-function filter_tint()
-{
-    tint_colour = document.getElementById("colour_name").value;
+    fill(0, 0, 255);
+    rect(45, 90, 30, 300);
+    rect(90, 45, 460, 30);
+    rect(565, 90, 30, 300);
+    rect(90, 405, 460, 30);
 }
